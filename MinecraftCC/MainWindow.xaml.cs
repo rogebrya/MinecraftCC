f7bb7ba0cfcc90b67603381825dfdfebc6ffb667
gladshaw <48333628+gladshaw@users.noreply.github.com>
2020-02-18 22:59:04 -0700

    Models and biome generation

--- MinecraftCC/MainWindow.xaml.cs
+++ MinecraftCC/MainWindow.xaml.cs
@@ -20,6 +20,36 @@ namespace MinecraftCC {
 	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
+			InitializeMap();
 		}
+
+		Map map;
+
+		public void InitializeMap() {
+			map = new Map(10, 10, 0);
+			for(int i = 0; i < map.Cells.GetUpperBound(0); i++) {
+				game.RowDefinitions.Add(new RowDefinition());
+				for(int j = 0; j < map.Cells.GetUpperBound(1); j++) {
+					game.ColumnDefinitions.Add(new ColumnDefinition());
+					TextBlock tb = new TextBlock();
+                    tb.Name = "tb_" + j + "_" + i;
+                    tb.TextWrapping = TextWrapping.Wrap;
+                    tb.TextAlignment = TextAlignment.Center;
+                    //tb.Background = Brushes.Black;
+                    Border border = new Border();
+                    border.BorderBrush = new SolidColorBrush(Colors.Black);
+                    border.BorderThickness = new Thickness(1);
+					tb.Background = map.Cells[i, j].Biome.Color;
+                    game.Children.Add(tb);
+                    Grid.SetRow(tb, i);
+                    Grid.SetColumn(tb, j);
+                    Grid.SetColumn(border, j);
+                    Grid.SetRow(border, i);
+                    game.Children.Add(border);
+                }
+			}
+
+		}
+		
 	}
 }
